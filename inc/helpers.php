93bef0a5def518de1dfba0efa5556df2a3e74593
Giacomo Rizzotti <giacomo.rizzotti@gmail.com>
2026-02-05 17:15:32 +0100

    Working on fonts

--- inc/helpers.php
+++ inc/helpers.php
@@ -190,6 +190,7 @@ function mini_get_google_fonts() {
             'Lato' => 'Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900',
             'Roboto' => 'Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900',
             'Poppins' => 'Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900',
+            'Reddit Sans' => 'Reddit+Sans:ital,wght@0,200..900;1,200..900',
         ],
         'secondary' => [
             'Oswald' => 'Oswald:wght@200..700',
@@ -208,6 +209,7 @@ function mini_get_google_fonts() {
             'Courier Prime' => 'Courier+Prime:ital,wght@0,400;0,700;1,400;1,700',
             'Roboto Mono' => 'Roboto+Mono:ital,wght@0,100..700;1,100..700',
             'Source Code Pro' => 'Source+Code+Pro:ital,wght@0,200..900;1,200..900',
+            'Reddit Mono' => 'Reddit+Mono:wght@200..900',
         ],
         'handwriting' => [
             'Indie Flower' => 'Indie+Flower',
