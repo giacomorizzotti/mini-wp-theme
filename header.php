b9ec8c0dff6613605b251abc47e74b6abf89f227
Giacomo Rizzotti <giacomo.rizzotti@gmail.com>
2026-02-10 17:28:16 +0100

    Google analytics

--- header.php
+++ header.php
@@ -150,12 +150,14 @@ if (
 ) {
 	$ga_code = mini_get_option( 'mini_analytics_options', 'mini_google_analytics_code' );
 ?>
+	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr( $ga_code ); ?>"></script>
 	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag("js", new Date());
-		gtag("config", "<?php echo esc_js( $ga_code ); ?>");
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', '<?php echo esc_js( $ga_code ); ?>');
 	</script>
 <?php
 }
