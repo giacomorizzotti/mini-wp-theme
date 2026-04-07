87e783ca5606684459a41ebe846ba69551a0a45f
Giacomo Rizzotti <giacomo.rizzotti@gmail.com>
2026-04-07 18:55:38 +0200

    Adding titillium web font

--- inc/helpers.php
+++ inc/helpers.php
@@ -175,6 +175,7 @@ function mini_get_google_fonts() {
             'Archivo Black' => 'Archivo+Black',
             'Raleway' => 'Raleway:ital,wght@0,100..900;1,100..900',
             'Inter' => 'Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900',
+            'Titillium Web' => 'Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700',
         ],
         'secondary' => [
             'Barlow' => 'Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900',
@@ -189,6 +190,7 @@ function mini_get_google_fonts() {
             'Archivo Black' => 'Archivo+Black',
             'Raleway' => 'Raleway:ital,wght@0,100..900;1,100..900',
             'Inter' => 'Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900',
+            'Titillium Web' => 'Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700',
         ],
         'serif' => [
             'Crimson Pro' => 'Crimson+Pro:ital,wght@0,200..900;1,200..900',
